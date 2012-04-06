9d9eae05f8cebd04248369a2af6a6165ba9cc6d7
Bhavya Juneja <bhavya.juneja42@gmail.com>
2012-04-07 00:10:26 +0530

    file input working

---
+++ parser.tab.h
@@ -0,0 +1,128 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     TOKEN_PRINT = 258,
+     TOKEN_IDENTIFIER = 259,
+     TOKEN_BEGIN = 260,
+     TOKEN_END = 261,
+     TOKEN_LOGICAL_VALUE = 262,
+     TOKEN_OPERATOR = 263,
+     TOKEN_OR = 264,
+     TOKEN_PROCEDURE = 265,
+     TOKEN_PLUS = 266,
+     TOKEN_MINUS = 267,
+     TOKEN_DIVIDE = 268,
+     TOKEN_POWER = 269,
+     TOKEN_MULTIPLY = 270,
+     TOKEN_COMMA = 271,
+     TOKEN_UNDERSCORE = 272,
+     TOKEN_SPECIFIER = 273,
+     TOKEN_LIBRARY = 274,
+     TOKEN_BRACKET = 275,
+     TOKEN_OPEN_BRACKET = 276,
+     TOKEN_CLOSE_BRACKET = 277,
+     TOKEN_OPEN_SQUARE_BRACKET = 278,
+     TOKEN_CLOSE_SQUARE_BRACKET = 279,
+     TOKEN_CONTINUE = 280,
+     TOKEN_REAL_NUM = 281,
+     TOKEN_INTEGER = 282,
+     TOKEN_COLON = 283,
+     TOKEN_REL_OP = 284,
+     TOKEN_LESS_EQUAL = 285,
+     TOKEN_GREATER = 286,
+     TOKEN_GREATER_EQUAL = 287,
+     TOKEN_EQUAL = 288,
+     TOKEN_LESS = 289,
+     TOKEN_NOTEQUAL = 290,
+     TOKEN_EQUIV = 291,
+     TOKEN_AND_OP = 292,
+     TOKEN_OR_OP = 293,
+     TOKEN_NOT_OP = 294,
+     TOKEN_GOTO = 295,
+     TOKEN_FOR = 296,
+     TOKEN_DO = 297,
+     TOKEN_WHILE = 298,
+     TOKEN_STEP = 299,
+     TOKEN_UNTIL = 300,
+     TOKEN_IF = 301,
+     TOKEN_THEN = 302,
+     TOKEN_ELSE = 303,
+     TOKEN_SWITCH = 304,
+     TOKEN_VALUE = 305,
+     TOKEN_BOOLEAN = 306,
+     TOKEN_TYPE_OWN = 307,
+     TOKEN_TYPE_INTEGER = 308,
+     TOKEN_TYPE_REAL = 309,
+     TOKEN_TYPE_BOOLEAN = 310,
+     TOKEN_TYPE_ARRAY = 311,
+     TOKEN_TYPE_SWITCH = 312,
+     TOKEN_TYPE_PROCEDURE = 313,
+     TOKEN_TYPE_STRING = 314,
+     TOKEN_TYPE_LABEL = 315,
+     TOKEN_TYPE_VALUE = 316,
+     TOKEN_ARRAY = 317,
+     TOKEN_IMPLY = 318,
+     TOKEN_SEMICOLON = 319,
+     TOKEN_LTRSTRING = 320,
+     TOKEN_TINTEGER = 321,
+     TOKEN_TREAL = 322,
+     TOKEN_RETURN = 323,
+     TOKEN_ASSIGN_IDENTIFIER = 324,
+     TOKEN_LABEL_IDENTIFIER = 325,
+     TOKEN_BOOL_IDENTIFIER = 326,
+     TOKEN_ASSIGN = 327,
+     TOKEN_NOT_EQUAL = 328,
+     TOKEN_RELOP = 329
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
---
+++ y.tab.h
@@ -0,0 +1,202 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     TOKEN_PRINT = 258,
+     TOKEN_IDENTIFIER = 259,
+     TOKEN_BEGIN = 260,
+     TOKEN_END = 261,
+     TOKEN_LOGICAL_VALUE = 262,
+     TOKEN_OPERATOR = 263,
+     TOKEN_OR = 264,
+     TOKEN_PROCEDURE = 265,
+     TOKEN_PLUS = 266,
+     TOKEN_MINUS = 267,
+     TOKEN_DIVIDE = 268,
+     TOKEN_POWER = 269,
+     TOKEN_MULTIPLY = 270,
+     TOKEN_COMMA = 271,
+     TOKEN_UNDERSCORE = 272,
+     TOKEN_SPECIFIER = 273,
+     TOKEN_LIBRARY = 274,
+     TOKEN_BRACKET = 275,
+     TOKEN_OPEN_BRACKET = 276,
+     TOKEN_CLOSE_BRACKET = 277,
+     TOKEN_OPEN_SQUARE_BRACKET = 278,
+     TOKEN_CLOSE_SQUARE_BRACKET = 279,
+     TOKEN_CONTINUE = 280,
+     TOKEN_REAL_NUM = 281,
+     TOKEN_INTEGER = 282,
+     TOKEN_COLON = 283,
+     TOKEN_REL_OP = 284,
+     TOKEN_LESS_EQUAL = 285,
+     TOKEN_GREATER = 286,
+     TOKEN_GREATER_EQUAL = 287,
+     TOKEN_EQUAL = 288,
+     TOKEN_LESS = 289,
+     TOKEN_NOTEQUAL = 290,
+     TOKEN_EQUIV = 291,
+     TOKEN_AND_OP = 292,
+     TOKEN_OR_OP = 293,
+     TOKEN_NOT_OP = 294,
+     TOKEN_GOTO = 295,
+     TOKEN_FOR = 296,
+     TOKEN_DO = 297,
+     TOKEN_WHILE = 298,
+     TOKEN_STEP = 299,
+     TOKEN_UNTIL = 300,
+     TOKEN_IF = 301,
+     TOKEN_THEN = 302,
+     TOKEN_ELSE = 303,
+     TOKEN_SWITCH = 304,
+     TOKEN_VALUE = 305,
+     TOKEN_BOOLEAN = 306,
+     TOKEN_TYPE_OWN = 307,
+     TOKEN_TYPE_INTEGER = 308,
+     TOKEN_TYPE_REAL = 309,
+     TOKEN_TYPE_BOOLEAN = 310,
+     TOKEN_TYPE_ARRAY = 311,
+     TOKEN_TYPE_SWITCH = 312,
+     TOKEN_TYPE_PROCEDURE = 313,
+     TOKEN_TYPE_STRING = 314,
+     TOKEN_TYPE_LABEL = 315,
+     TOKEN_TYPE_VALUE = 316,
+     TOKEN_ARRAY = 317,
+     TOKEN_IMPLY = 318,
+     TOKEN_SEMICOLON = 319,
+     TOKEN_LTRSTRING = 320,
+     TOKEN_TINTEGER = 321,
+     TOKEN_TREAL = 322,
+     TOKEN_RETURN = 323,
+     TOKEN_ASSIGN_IDENTIFIER = 324,
+     TOKEN_LABEL_IDENTIFIER = 325,
+     TOKEN_BOOL_IDENTIFIER = 326,
+     TOKEN_ASSIGN = 327,
+     TOKEN_NOT_EQUAL = 328,
+     TOKEN_RELOP = 329
+   };
+#endif
+/* Tokens.  */
+#define TOKEN_PRINT 258
+#define TOKEN_IDENTIFIER 259
+#define TOKEN_BEGIN 260
+#define TOKEN_END 261
+#define TOKEN_LOGICAL_VALUE 262
+#define TOKEN_OPERATOR 263
+#define TOKEN_OR 264
+#define TOKEN_PROCEDURE 265
+#define TOKEN_PLUS 266
+#define TOKEN_MINUS 267
+#define TOKEN_DIVIDE 268
+#define TOKEN_POWER 269
+#define TOKEN_MULTIPLY 270
+#define TOKEN_COMMA 271
+#define TOKEN_UNDERSCORE 272
+#define TOKEN_SPECIFIER 273
+#define TOKEN_LIBRARY 274
+#define TOKEN_BRACKET 275
+#define TOKEN_OPEN_BRACKET 276
+#define TOKEN_CLOSE_BRACKET 277
+#define TOKEN_OPEN_SQUARE_BRACKET 278
+#define TOKEN_CLOSE_SQUARE_BRACKET 279
+#define TOKEN_CONTINUE 280
+#define TOKEN_REAL_NUM 281
+#define TOKEN_INTEGER 282
+#define TOKEN_COLON 283
+#define TOKEN_REL_OP 284
+#define TOKEN_LESS_EQUAL 285
+#define TOKEN_GREATER 286
+#define TOKEN_GREATER_EQUAL 287
+#define TOKEN_EQUAL 288
+#define TOKEN_LESS 289
+#define TOKEN_NOTEQUAL 290
+#define TOKEN_EQUIV 291
+#define TOKEN_AND_OP 292
+#define TOKEN_OR_OP 293
+#define TOKEN_NOT_OP 294
+#define TOKEN_GOTO 295
+#define TOKEN_FOR 296
+#define TOKEN_DO 297
+#define TOKEN_WHILE 298
+#define TOKEN_STEP 299
+#define TOKEN_UNTIL 300
+#define TOKEN_IF 301
+#define TOKEN_THEN 302
+#define TOKEN_ELSE 303
+#define TOKEN_SWITCH 304
+#define TOKEN_VALUE 305
+#define TOKEN_BOOLEAN 306
+#define TOKEN_TYPE_OWN 307
+#define TOKEN_TYPE_INTEGER 308
+#define TOKEN_TYPE_REAL 309
+#define TOKEN_TYPE_BOOLEAN 310
+#define TOKEN_TYPE_ARRAY 311
+#define TOKEN_TYPE_SWITCH 312
+#define TOKEN_TYPE_PROCEDURE 313
+#define TOKEN_TYPE_STRING 314
+#define TOKEN_TYPE_LABEL 315
+#define TOKEN_TYPE_VALUE 316
+#define TOKEN_ARRAY 317
+#define TOKEN_IMPLY 318
+#define TOKEN_SEMICOLON 319
+#define TOKEN_LTRSTRING 320
+#define TOKEN_TINTEGER 321
+#define TOKEN_TREAL 322
+#define TOKEN_RETURN 323
+#define TOKEN_ASSIGN_IDENTIFIER 324
+#define TOKEN_LABEL_IDENTIFIER 325
+#define TOKEN_BOOL_IDENTIFIER 326
+#define TOKEN_ASSIGN 327
+#define TOKEN_NOT_EQUAL 328
+#define TOKEN_RELOP 329
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
